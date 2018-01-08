32d15600a46bcc5faba55de7d019a4160c341f87
hareesh114 <30810480+hareesh114@users.noreply.github.com>
2018-01-08 23:32:55 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,137 +1,168 @@
 import random
-import sys 
+import sys
+import os
+import datetime
 
-#pip install pillow
+now = datetime.datetime.now()
+
 
 from PIL import Image                                                                                
 im = Image.open('1.jpeg')
-img=im.resize((256,512))
-img.show() 
+img=im.resize((512,512))
+img.show()
 
-#printing 100 numbers
-
-b=list(range(11,21))[::-1]
-c=list(range(21,31))
-d=list(range(31,41))[::-1]
-e=list(range(41,51))
-f=list(range(51,61))[::-1]
-g=list(range(61,71))
-h=list(range(71,81))[::-1]
-i=list(range(81,91))
-j=list(range(91,101))[::-1]
+print("\n            -----------------------")
+print(now.strftime("             %d-%m-%Y | %H:%M:%S"))
+print("            -----------------------")
 
 print("\n 	     SNAKE && LADDER GAME")
 print("            ----------------------")
 print("        Snakes and ladders is still fun!")
 print("		               -INDIA MART\n")
-print("\n* * * * * * * * * * * * * * * * * * * * * * *")
-print('*',j,'*','\n','*',i,'*','\n','*',h,'*','\n','*',g,'*','\n','*',f,'*','\n','*',e,'*','\n','*',d,'*','\n','*',c,'*','\n','*',b,'*')
 
-print(' ',end="")
-print('*',end="")
-print(' ',end="")
-print("[",end="")
-for i in range(1,10):
-	print('0',end="")
-	print(i,end="")
-	print(',',end="")
-	print(' ',end="") 
-print("10",end="")
-print("]",end="")
-print(' ',end="")
-print('*')
-print("* * * * * * * * * * * * * * * * * * * * * * *")
+print("IMPORTANT INSTRUCTIONS:")
+print("\n***Playing the Snakes and Ladders Board Game***\n")
+print("->Understand the object of the game")
+print("->Decide who goes first")
+print("->Roll the dice and move")
+print("->Climb up ladders")
+print("->Slide down snakes")
+print("->Take an extra turn if you roll a six")
+print("->Land exactly on the last square to win") 
+print("\nNOTE:-Number of players must be > 1")
 
 
-def play():
-	player1=0
-	player2=0
-
-	#positions of snackes and ladders
-	def check_for_snakes_and_ladders(n):
-
-		"""This method checks for the presence of snakes or ladders in the board"""
-	
-		ladders={1:38,4:14,9:31,21:42,28:84,36:44,51:67,71:91,80:97}
-		snakes={98:78,95:75,93:73,87:24,64:60,62:19,56:53,49:11,48:26,16:6}
-		if(n in ladders):
-			print("\n-----------Hurray..!!Its a ladder,Climb up-----------")
-			n=ladders[n]
-		elif(n in snakes):
-			print("\n-----------OOPS..!!Its a snake,you are swallowed!!,Come down-----------")
-			n=snakes[n]
-		return n
+sAlPoS={1: 38,4: 14,9:  31,17: 7,21: 42,28: 84,51: 67,54: 34,62: 19,64: 60,72: 91,80: 99,87: 36,93: 73,95: 75,98: 79,}
 
 
-	#rolling dice
-		"""This method takes input from each of the players, prints the current position of the players and checks for the
-		winner of the game"""
+class Player:
+	def __init__(self, inPlayerNum):
+		self.playerPos = 0
+		self.playerNum = inPlayerNum
+
+	def updatePosition(self, inPos):
+		self.playerPos = inPos
+
+	def getPosition(self):
+		return self.playerPos
+
+	def getPlayerNum(self):
+		return self.playerNum
 
 
-	def roll_dice(r):	
-		print("click y to roll die // click n to exit game")
-		c=input() 
-		d=random.randint(1,6)
-		if(c=="y" or c=="Y"):
-			k=d
-			d=int(d)+int(r) 
-			print("number on die is :",end="")
-			print(k)
-		elif(c=="n"  or c=="N"):
-			sys.exit()
-		return d
+def gameMaster(inPlayer):
+	global winner
+	if winner == False:
+		print("\n-----------Player %i-----------" % inPlayer.getPlayerNum())
+		roll = rollDice()
+		print("You rolled: %i" % roll)
+		movePlayer(inPlayer, roll)
+		checkPosition(inPlayer)
+		if roll is 6:
+			print("CONGO..ONE MORE CHANCE MATE..!!")
+			gameMaster(playerList[i])  
 
 
-	while(player1 < 100 or player2 < 100):
-		print("\n	Its turn of player1\n")
-		prev1=player1
-		player1=roll_dice(player1)
-		player1=check_for_snakes_and_ladders(player1)
-		print("\n************************************************")
-		k1=player1 
-		if(k1<=100):
-			print("Current status of Player1:",player1,"and Player2:",player2)
+def rollDice():   
+    print("click y to roll die || click n to exit game") 
+    c=input() 
+    if(c=='y' or c=='Y'):
+        d=random.randint(1,6)
+    elif(c=='n'  or c=='N'):
+        sys.exit()
+    else:
+    	os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 1,1000))
+    	print("Sorry!  That was no valid option.  Try again...\n")
+    	f=rollDice()
+    	return f
+    return d
+
+
+count=1
+def movePlayer(inPlayer, roll):
+	global playerList
+	if inPlayer.getPosition() + roll <= 100:
+		inPlayer.updatePosition(inPlayer.getPosition() + roll)
+		print("You are at spot %i" % inPlayer.getPosition())
+		if inPlayer.getPosition() == 100:
+			global count
+			print("\nCongratulations...Player %i is the Winner-"% inPlayer.getPlayerNum(),end="")
+			print(" %i "% count)
+			count=count+1
+			playerList.remove(inPlayer)
+			global looser
+			looser=True
+			if(len(playerList)==1 or len(playerList)==0):
+				global winner
+				winner = True
+	else:
+		print("HardLuck..!!Wait For Next Chance")
+		print("You are at spot %i" % inPlayer.getPosition()) 
+
+
+def checkPosition(inPlayer):
+	for pos in sAlPoS:
+		if pos == inPlayer.getPosition():
+			if pos < sAlPoS[pos]:
+				print("Hurray..!!Its a ladder,you Climb up to spot %i" % sAlPoS[pos])
+			else:
+				print("OOPS..!!Its a snake,you are swallowed!!,Come down to spot %i" % sAlPoS[pos])
+			inPlayer.updatePosition(sAlPoS[pos])
+
+
+if __name__ == '__main__':
+	global winner
+	global looser
+	winner = False		
+	looser = False
+	while True:
+        	try:
+        	    numPlayers = int(input('\nEnter number of players: '))
+        	    if numPlayers is 1:
+        	    	raise ValueError
+        	    else:
+        	    	break
+        	except ValueError:
+        	    os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 1,1000))
+        	    print("Oops!  That was no valid number.  Try again...")
+	num=numPlayers
+	print("\nThe Players Are:")
+	j=1
+	while(num!=0):
+		print("player %i" % j )
+		j=j+1
+		num=num-1
 		
-			print("\n************************************************")
-			if(k1 is 100):
-				print("\n************************************************")
-				print(" 	Congrats player1, you are the Winner of the game")
-				break
-		elif(k1>100):
-			player1=prev1
-			print(" 	HardLuck..!!Wait For Next Chance") 
-			print("Current status of Player1:",player1,"and Player2:",player2)
-			print("************************************************\n")
+	global playerList		
+	playerList = []
 	
-		print("\n	Its turn of player2\n")
-		prev2=player2
-		player2=roll_dice(player2)
-		player2=check_for_snakes_and_ladders(player2)
-		print("\n************************************************")	
-		k2=player2
-		if(k2<=100):	
-			print("Current status of Player1:",player1," and Player2:",player2)
-		
-			print("\n************************************************")
-			if(k2 is 100):
-				print("\n************************************************")
-				print(" 	Congrats player2, you are the Winner of the game")
-				break
-		elif(k2>100):
-			player2=prev2
-			print(" 	HardLuck..!!Wait For Next Chance")
-			print("Current status of Player1:",player1,"and Player2:",player2)				
-			print("************************************************\n") 
+	for j in range(1,numPlayers+1): 
+		playerList.append(Player(j))
+	
+	while winner == False:
+		i=0
+		while(i<len(playerList)):
+			if winner == False:
+				gameMaster(playerList[i])
+			else:
+				break		 				
+			if looser == True:
+				looser=False
+				continue
+			i+=1	
+			
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 1,500))
+	
+def python():				
+	print("\nClick 1 to new game || click 0 to quit")
+	q=input() 
+	if(q=='1'):
+		os.system("clear & python3 py.py")
+	elif(q=='0'):
+		exit()
+	else:
+		os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 1,1000))
+		print("Choose Correct Option")
+		python()		
 
-play()
-
-
-f=0
-while(f is 0):
-	print("Click x to new game || click z to quit")
-	i=input()
-	if(i=="x" or i=="X"):
-		play()
-	elif(i=="Z" or i=="z"):
-		sys.exit() 
-
+python() 
